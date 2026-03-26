b7cc1eca52be8a3eddb14dcae60ce8ba472ad787
umercdkee <146094120+umercdkee@users.noreply.github.com>
2026-03-26 09:32:23 +0500

    Time: 95 ms (9.77%), Space: 64 MB (5.02%) - LeetHub

---
+++ 0018-4sum/0018-4sum.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[][]}
+ */
+var fourSum = function(nums, target) {
+    nums.sort((a, b) => a - b);
+    let result = []
+    for (let i = 0; i < nums.length - 3; i++) {
+        for (let j = i + 1; j < nums.length - 2; j++) {
+            let left = j + 1;
+            let right = nums.length - 1
+            while (left < right){
+                let sum = nums[i] + nums[j] + nums[left] + nums[right];
+                if (sum === target){
+                    let key = [nums[i], nums[j], nums[left], nums[right]].sort((a, b) => a - b).toString();
+                    if (!result.some(x => x.toString() === key)) {
+                        result.push([nums[i], nums[j], nums[left], nums[right]].sort((a, b) => a - b));
+                    }
+                    while (left < right && nums[left] === nums[left + 1]) 
+                        left++;
+                    while (left < right && nums[right] === nums[right - 1]) 
+                        right--;
+                    left ++;
+                    right--;
+                }
+                else if (sum < target)
+                    left++;
+                else if (sum > target)
+                    right--;
+            }
+        }
+    }
+    return result
+};
