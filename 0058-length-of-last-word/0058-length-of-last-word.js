0778174047db15e39e00655c8a3c13ce23f7dccf
Umer Siddiqui <146094120+umercdkee@users.noreply.github.com>
2026-05-23 17:31:06 +0500

    Time: 0 ms (100%), Space: 54.1 MB (21.86%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    s
+    s = s.trim().split(" ");
+    console.log(s);
+    return s[s.length-1].length;
+};
